e00fe6e991ed230545f2fdd838e1e54d4f66dbd8
leonardo zapparoli <leo.zapparoli@gmail.com>
2016-12-13 15:39:13 -0200

    ml utils

---
+++ src/ml.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+from __future__ import division
+from collections import Counter
+import random
+import vector
+
+
+def split_data(data, prob):
+    """ divide os dados em grações [prob, 1 - prob] """
+    results = [],[]
+    for row in data:
+        results[0 if random.random() < prob else 1].append(row)
+    return results
+
+def train_test_split(x, y, test_pct):
+    data = zip(x, y)
+    train, test = split_data(data, 1 - test_pct)
+    x_train, y_train = zip(*train)
+    x_test, y_test = zip(*test)
+    return x_train, x_test, y_train, y_test
+
+def accuracy(tp, fp, fn, tn):
+    correct = tp + tn
+    total = tp + fp + fn + tn
+    return correct / total
+
+def precision(tp, fp, fn, tn):
+    return tp / (tp + fp)
+
+def recall(tp, fp, fn, tn):
+    return tp / (tp + fn)
+
+
+def f1_score(tp, fp, fn, tn):
+    p = precision(tp, fp, fn, tn)
+    r = recall(tp, fp, fn, tn)
+
+    return 2 * p * r / (p + r)
+
